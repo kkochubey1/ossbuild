6b9f43093ada8908bb902318c1e109c71c97fe4e
ylatuya <ylatuya@72a73a14-1cf0-11de-a027-6f5b8e30e268>
2010-03-09 16:08:15 +0000

    Update gst-plugins-good generated files

--- trunk/Main/GStreamer/Windows/Generated/gst-plugins-good/gst/rtpmanager/gstrtpbin-marshal.c
+++ trunk/Main/GStreamer/Windows/Generated/gst-plugins-good/gst/rtpmanager/gstrtpbin-marshal.c
@@ -318,3 +318,44 @@ gst_rtp_bin_marshal_VOID__OBJECT_OBJECT (GClosure     *closure,
             data2);
 }
 
+/* UINT64:BOOL,UINT64 */
+void
+gst_rtp_bin_marshal_UINT64__BOOLEAN_UINT64 (GClosure     *closure,
+                                            GValue       *return_value G_GNUC_UNUSED,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint G_GNUC_UNUSED,
+                                            gpointer      marshal_data)
+{
+  typedef guint64 (*GMarshalFunc_UINT64__BOOLEAN_UINT64) (gpointer     data1,
+                                                          gboolean     arg_1,
+                                                          guint64      arg_2,
+                                                          gpointer     data2);
+  register GMarshalFunc_UINT64__BOOLEAN_UINT64 callback;
+  register GCClosure *cc = (GCClosure*) closure;
+  register gpointer data1, data2;
+  guint64 v_return;
+
+  g_return_if_fail (return_value != NULL);
+  g_return_if_fail (n_param_values == 3);
+
+  if (G_CCLOSURE_SWAP_DATA (closure))
+    {
+      data1 = closure->data;
+      data2 = g_value_peek_pointer (param_values + 0);
+    }
+  else
+    {
+      data1 = g_value_peek_pointer (param_values + 0);
+      data2 = closure->data;
+    }
+  callback = (GMarshalFunc_UINT64__BOOLEAN_UINT64) (marshal_data ? marshal_data : cc->callback);
+
+  v_return = callback (data1,
+                       g_marshal_value_peek_boolean (param_values + 1),
+                       g_marshal_value_peek_uint64 (param_values + 2),
+                       data2);
+
+  g_value_set_uint64 (return_value, v_return);
+}
+
