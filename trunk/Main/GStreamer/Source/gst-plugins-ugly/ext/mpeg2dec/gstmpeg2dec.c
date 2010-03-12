ac3da4ad2a4bfd3a141ad6beb797d822e50cc21f
ylatuya <ylatuya@72a73a14-1cf0-11de-a027-6f5b8e30e268>
2010-03-12 22:39:40 +0000

    Fix gstmpeg2dec compilation (Added patch)

--- trunk/Main/GStreamer/Source/gst-plugins-ugly/ext/mpeg2dec/gstmpeg2dec.c
+++ trunk/Main/GStreamer/Source/gst-plugins-ugly/ext/mpeg2dec/gstmpeg2dec.c
@@ -972,11 +972,11 @@ handle_slice (GstMpeg2dec * mpeg2dec, const mpeg2_info_t * info)
       GST_TIME_FORMAT,
       (picture->flags & PIC_FLAG_PROGRESSIVE_FRAME ? "prog" : "    "),
       (picture->flags & PIC_FLAG_TOP_FIELD_FIRST ? "tff" : "   "),
-#if MPEG2_RELEASE >= MPEG2_VERSION(0,5,0)
+//#if MPEG2_RELEASE >= MPEG2_VERSION(0,5,0)
       (picture->flags & PIC_FLAG_REPEAT_FIRST_FIELD ? "rff" : "   "),
-#else
+/*#else
       "unknown rff",
-#endif
+#endif*/
       (picture->flags & PIC_FLAG_SKIP ? "skip" : "    "),
       (picture->flags & PIC_FLAG_COMPOSITE_DISPLAY ? "composite" : "         "),
       picture->nb_fields, GST_BUFFER_OFFSET (outbuf),
