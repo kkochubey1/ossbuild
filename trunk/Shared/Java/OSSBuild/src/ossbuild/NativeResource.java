7e269f7c699ef78486f89ed12c8dbfde7783d8dc
david.g.hoyt <david.g.hoyt@72a73a14-1cf0-11de-a027-6f5b8e30e268>
2010-03-13 01:05:26 +0000

    Java updates. Refactored some method names, fixed missing resource bug.

---
+++ trunk/Shared/Java/OSSBuild/src/ossbuild/NativeResource.java
@@ -0,0 +1,31 @@
+
+package ossbuild;
+
+/**
+ *
+ * @author David Hoyt <dhoyt@hoytsoft.org>
+ */
+public class NativeResource {
+	public static final String
+		  Base                      = "base"
+		, XML                       = "xml"
+		, GLib                      = "glib"
+		, Fonts                     = "fonts"
+		, Images                    = "images"
+		, SDL                       = "sdl"
+		, Graphics                  = "graphics"
+		, FFmpeg                    = "ffmpeg"
+		, OpenGL                    = "opengl"
+		, Crypto                    = "crypto"
+
+		, GStreamer                 = "gstreamer"
+		//, GStreamerPluginsBase      = "gst-plugins-base"
+		//, GStreamerPluginsGood      = "gst-plugins-good"
+		//, GStreamerPluginsBad       = "gst-plugins-bad"
+		//, GStreamerPluginsUgly      = "gst-plugins-ugly"
+		//, GStreamerPluginsGL        = "gst-plugins-gl"
+		//, GStreamerFFmpeg           = "gst-ffmpeg"
+		//, GStreamerNonLin           = "gnonlin"
+		//, Farsight                  = "farsight"
+	;
+}
