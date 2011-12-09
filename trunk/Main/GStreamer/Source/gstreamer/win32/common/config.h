b5092bde232578fd93b7358a37417b5e02e4f400
david.g.hoyt <david.g.hoyt@72a73a14-1cf0-11de-a027-6f5b8e30e268>
2011-12-09 00:34:43 +0000

    Fix gstreamer win32 config.h

--- trunk/Main/GStreamer/Source/gstreamer/win32/common/config.h
+++ trunk/Main/GStreamer/Source/gstreamer/win32/common/config.h
@@ -62,7 +62,7 @@
 #define GST_PACKAGE_NAME "GStreamer source release"
 
 /* package origin */
-#define GST_PACKAGE_ORIGIN "Unknown package origin"
+#define GST_PACKAGE_ORIGIN "http://code.google.com/p/ossbuild/"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
 #define GST_PACKAGE_RELEASE_DATETIME "2011-06-15"
